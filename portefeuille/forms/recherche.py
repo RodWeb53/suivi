0ae6f77b3667789edc9e74c4a082b5bb934c73f7
Rodolphe R. CLEMENCEAU <RCLEMENCEAU@slts-asp.local>
2024-06-15 09:56:08 +0200

    mise en place

--- portefeuille/forms/recherche.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-# Modéle pour le formulaire de recherche dans le suivi
-class ClientSearchForm(forms.Form):
-    client = forms.CharField(label="Numéro du client", required=True)
-    produit = forms.CharField(label="Numéro du produit", required=True)
-    commande = forms.CharField(label="Numéro de commande", required=True)
-    montant = forms.CharField(label="Montant sup à", required=True)
--- portefeuille/views/home.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-import datetime
-import os.path
-import time
-
-
-def index(request):
-    date_fichier = datetime.datetime.strptime(time.ctime(os.path.getmtime("data.json")), "%a %b %d %H:%M:%S %Y")
-    return render(request, "portefeuille/home.html", locals())
