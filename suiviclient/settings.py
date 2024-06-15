cc0dff5e0a80f85d294f7cb11fac6996b03ce64f
Rodolphe R. CLEMENCEAU <RCLEMENCEAU@slts-asp.local>
2024-06-15 17:11:51 +0200

    changement mineur

--- suiviclient/settings.py
+++ suiviclient/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pxz77wu9*m6s^m($nckwh$kljjy!2#u#6cs-+-t9mtekiyfd=7'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = []
 
