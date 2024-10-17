b43ff999618c8e1a6a78b775ddb7ff978f1ac35b
Rodolphe R. CLEMENCEAU <RCLEMENCEAU@slts-asp.local>
2024-10-17 07:04:23 +0200

    MAJ Data le 17/10/2024

--- portefeuille/forms/contrainte.py
+++ portefeuille/forms/contrainte.py
@@ -3,5 +3,15 @@ from django import forms
 
 # Modéle pour le formulaire de recherche dans le suivi
 class ContrainteSearchForm(forms.Form):
+    CHOICES = [
+        ('0', 'Complet'),
+        ('1', 'Opération restante'),
+        ('2', 'Sans opérations'),
+    ]
+    affichage_choix = forms.ChoiceField(
+        label="Choix d'affichage",
+        widget=forms.RadioSelect,
+        choices=CHOICES,
+    )
     contrainte = forms.CharField(label="Contrainte", required=False)
     sem_contrainte = forms.CharField(label="Semaine contrainte", required=False)
--- portefeuille/views/contrainte.py
+++ portefeuille/views/contrainte.py
@@ -8,7 +8,7 @@ import json
 
 def contrainte(request):
     """
-        Fonction pour afficher les données avec les filtres de la page de recherche opération
+        Fonction pour afficher les données avec les filtres de la page de recherche contrainte
     """
     # Ouverture du fichier JSON
     with open('data.json', 'r') as f:
@@ -27,11 +27,14 @@ def contrainte(request):
         form = ContrainteSearchForm()
         contrainte_form = request.GET.get("contrainte", 0)
         sem_contrainte_form = request.GET.get("sem_contrainte", 0)
+        affichage_choix_form = request.GET.get("affichage_choix", 0)
+        affichage_detail = affichage_choix_form
 
         if contrainte_form == 0:
             # Chargement des données de recherche dans le formulaire de base avec des valeurs à 0
             form.fields['contrainte'].initial = contrainte_form
             form.fields['sem_contrainte'].initial = sem_contrainte_form
+            form.fields['affichage_choix'].initial = affichage_choix_form
             # Envoie des données globale
             data = data
         else:
@@ -40,6 +43,7 @@ def contrainte(request):
             # Chargement des valeurs du filtre dans la page de base
             form.fields['contrainte'].initial = contrainte_form
             form.fields['sem_contrainte'].initial = sem_contrainte_form
+            form.fields['affichage_choix'].initial = affichage_choix_form
             # S'il y a un filtre sur la contrainte
             if contrainte_form != "0" and sem_contrainte_form == "0":
 
@@ -50,6 +54,7 @@ def contrainte(request):
                                 liste_filtre.append(client)
 
                 data = liste_filtre
+                affichage_detail = affichage_choix_form
             # S'il y a un filtre sur la semaine
             elif contrainte_form == "0" and int(sem_contrainte_form) != 0:
 
@@ -60,6 +65,7 @@ def contrainte(request):
                                 liste_filtre.append(client)
 
                 data = liste_filtre
+                affichage_detail = affichage_choix_form
             # S'il y a un filtre sur la semaine et la contrainte
             elif contrainte_form != "0" and int(sem_contrainte_form) != 0:
 
@@ -71,13 +77,15 @@ def contrainte(request):
                                 liste_filtre.append(client)
 
                 data = liste_filtre
+                affichage_detail = affichage_choix_form
 
     # Gestion de la pagination des pages
     paginator = Paginator(data, 15)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'portefeuille/contrainte.html', context={"page_obj": page_obj, "form": form})
+    return render(request, 'portefeuille/contrainte.html', context={"page_obj": page_obj, "form": form,
+                                                                    "type_affichage": affichage_detail})
 
 
 def list_operation(request):
